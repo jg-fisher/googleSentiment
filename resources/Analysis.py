801b78f7cf012dc233e8e63918e9c48378480e1b
John Fisher <johnfisher@iMac-3.local>
2018-04-29 23:16:22 -0700

    cleanup

--- resources/Analysis.py
+++ resources/Analysis.py
@@ -7,16 +7,12 @@ class Analysis:
 		self.term = term
 		self.sentiment = 0
 		self.subjectivity = 0
-		
 		self.url = 'https://www.google.com/search?q={0}&source=lnms&tbm=nws'.format(self.term)
 
 	def run(self):
 		response = requests.get(self.url)
-		
 		soup = BeautifulSoup(response.text, 'html.parser')
-		
 		headline_results = soup.find_all('div', class_='st')
-		
 		for text in headline_results:
 			blob = TextBlob(text.get_text())	
 			self.sentiment += blob.sentiment.polarity / len(headline_results)
